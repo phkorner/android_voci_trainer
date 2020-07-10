fe54d9ca944bc95015edeef5b4d39d4c4577beb4
phkorner <Philipp@140183>
2020-07-10 16:51:12 +0200

    update

--- app/src/main/java/com/example/voci_trainer/MainActivity.kt
+++ app/src/main/java/com/example/voci_trainer/MainActivity.kt
@@ -14,14 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        Confirm.setOnClickListener {
-            setContentView(R.layout.fragment_first)
-            setSupportActionBar(findViewById(R.id.toolbar))
-        }
-   }
-
-
-
+    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
